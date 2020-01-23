7036c5c76fb3e98fa127f25d00281bd08eaf8b0a
Lihe Liu <lihe.liu@ufl.edu>
2020-01-23 10:19:53 -0500

    pre msig db

--- Get_Msig_DB.R
+++ Get_Msig_DB.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 # specify database location
 url_template = 'http://software.broadinstitute.org/gsea/msigdb/cards/'
 Msig_db_destination = '/ufrc/penagaricano/lihe.liu/Methylation_WGCNA/Msig_db'
@@ -25,23 +26,43 @@ Get_Descrip = function(URL,
   message('try on ', URL)
   return(final)
 }
-# loop to retrive
-#Msig_urls_t = Msig_urls[1:20]
-All_Descrip = c()
-for (i in seq_along(Msig_urls)){
-  #message(i)
-  All_Descrip[i] = Get_Descrip(Msig_urls[i])
-}
-
-# massage
-#Msig_name_index_t = Msig_name_index[1:20]
-Universe_Descrip = data.frame(cbind(Msig_name_index,All_Descrip))
-colnames(Universe_Descrip) = c('gs_name','gd_description')
-
-m_df_all = dplyr::left_join(m_df,
-                            Universe_Descrip,
-                            by = c("gs_name" = "gs_name"))
 
 setwd(Msig_db_destination)
-save(m_df_all,file = "Msigdb_bta.RData")
+load('Msigdb_bta.RData')
+# # loop to retrive
+# #Msig_urls_t = Msig_urls[1:20]
+# All_Descrip = c()
+# for (i in seq_along(Msig_urls)){
+#   #message(i)
+#   All_Descrip[i] = Get_Descrip(Msig_urls[i])
+# }
+# 
+# # massage
+# #Msig_name_index_t = Msig_name_index[1:20]
+# Universe_Descrip = data.frame(cbind(Msig_name_index,All_Descrip))
+# colnames(Universe_Descrip) = c('gs_name','gd_description')
+# 
+# 
+# m_df_all = dplyr::left_join(m_df,
+#                             Universe_Descrip,
+#                             by = c("gs_name" = "gs_name"))
+# 
 
+DB_List = list()
+MsigRecords = unique(m_df_all[,c("gs_name","gd_description")]) %>% arrange(gs_name)
+MsigID = na.omit(MsigRecords$gs_name)
+MsigTerm = na.omit(MsigRecords$gd_description)
+for ( p in seq_along(MsigID)){
+  tmp = subset(m_df_all, gs_name == MsigID[p])$entrez_gene
+  DB_List[[p]] = tmp #
+  names(DB_List)[p]  <- paste(MsigID[p],"-")#MeshTerm[p]
+  print(p)
+}
+
+
+setwd(Msig_db_destination)
+save(m_df_all,
+     DB_List,
+     file = "Msigdb_bta.RData")
+
+# load('Msigdb_bta.RData')
